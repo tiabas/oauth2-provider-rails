29fdd39f1a79724d8b7a5d9945fc2c77d2764896
Kevin Mutyaba <tiabasnk@gmail.com>
2012-07-11 08:59:58 -0700

    Added Error Classes

--- app/controllers/oauth2_controller.rb
+++ app/controllers/oauth2_controller.rb
@@ -1,14 +1,19 @@
 class Oauth2Controller < ApplicationController
-	
+
 	before_filter :create_oauth2_client_request
 	before_filter :verify_oauth2_client_id
 	before_filter :verify_oauth2_response_type, :only => :authorize
 
   def authorize
-  	redirect_to @oauth2_client_request.authorization_redirect_uri, :status => :found
+  	# render authorization page
+  end
+
+  def process_authorization
+  	allow = params.fetch('allow', false) && true
+  	redirect_to @oauth2_client_request.authorization_redirect_uri(allow), :status => :found
   end
 
   def token
-
+  	
   end
 end
--- lib/oauth2/oauth2.rb
+++ lib/oauth2/oauth2.rb
@@ -2,59 +2,124 @@ require "addressable/uri"
 
 module OAUTH2
 
-  class InvalidRequest < StandardError
-    def initialize(msg = "You've triggered a MyError")
-      super(msg)
-    end
-  end
-  
-  class Request
-    
-    RESPONSE_TYPES = %w{ code token }
-    REDIRECT_URI_REGEX = /^((http|https):\/\/)[\w\d\.\/]+\.([a-z]+)/
+  class InvalidClient < StandardError;end
+  class InvalidRequest < StandardError;end
+  class InvalidScope < StandardError;end
+  class InvalidGrant < StandardError;end
+  class AccessDenied < StandardError;end
+  class UnsupportedResponseType < StandardError;end
+  class UnsupportedGrantType < StandardError;end
+  class UnauthorizedClient < StandardError;end
 
-    attr_reader :response_type, :client_id, :state, :scope
+  class Request
+    RESPONSE_TYPES = %w{ code token }
+    GRANT_TYPES = %w{ :authorization_code password token client_credentials }
+
+    attr_reader :response_type, :client_id, :state, :scope, :authenticated
     private attr_accessor :errors
     def initialize(opts={})
        @response_type = opts[:response_type]
        @client_id = opts[:client_id]
        @state = opts[:state]
+       @grant_type = opts[:grant_type]
        @scope = opts[:scope]
-       @redirect_uri = opts[:redirect_uri]
+       @username = opts[:username]
+       @password = opts[:password]
+       @redirect_uri = Addressable::URI.unencode opts[:redirect_uri]
        @errors = {}
     end
 
+    def authenticated?
+      return @authenticated unless @authenticated.nil?
+      authenticate_client
+    end
+    
     def redirect_uri
-      @redirect_uri || (client_application ? client_application.redirect_uri : nil )
+      @redirect_uri  ||= (client_application ? client_application.redirect_uri : nil )
+      unless redirect_uri_valid?
+        raise InvalidRequest, errors[:redirect_uri].join(" ")
+      end
+      @redirect_uri
     end
 
     def client_id_valid?
       validate_client_id
     end
 
+    def scope_valid?
+
+    end
+
     def response_type_valid?
       validate_response_type
     end
 
-    def redirect_uri_valid? 
+    def redirect_uri_valid?
       validate_redirect_uri
     end
 
-    def authorization_redirect_uri
-      unless redirect_uri_valid?
-        raise InvalidURIError, errors[:invalid_request].join " "
-      end
-      uri = Addressable::URI.parse(@redirect_uri)
-      params = uri.query_values
-      params[:code] = generate_authorization_code if response_type == "code"
-      params[:token] = generate_request_token if response_type == "token"
-      params[:state] = state unless state.nil?
-      uri.query_values = params
-      return uri
+    def authorization_redirect_uri(allow) 
+      build_response_uri authorization_response
+    end
+
+    def token_response_redirect_uri(allow) 
+      build_response_uri access_token_response
+    end
+
+    def access_token
+      # { :access_token => %%, :expires_in => %%, :token_type => %%}
+      generate_access_token
     end
 
   private
     
+    def client_application
+      return @client_application unless @client_application.nil?
+      validate_client_id
+    end
+
+    def build_response_uri(params={})
+      uri = Addressable::URI.parse(redirect_uri)
+      response_params = uri.query_values
+      response_params.merge! params
+      uri.query_values = response_params
+      return uri
+    end
+
+    def authorization_response
+      params = {}
+      params[:state] = state unless state.nil?
+      params[:code] = generate_authorization_code
+    end
+
+    def access_token_response
+      params = {}
+      params[:refresh_token] = generate_refresh_token if response_type == "code"
+      params.merge! access_token
+    end
+
+    def user_credentials_valid?
+      return false unless (@username && @password)
+      # User.check @username, @password
+    end
+
+    def authenticate_client
+      @authenticated = begin
+        case grant_type
+          when "client_credentials"
+            true if client_application.confidential?
+            @errors[:credentials] << "Unauthorized client"
+            false
+          when "password" 
+            true if user_credentials_valid?
+            @errors[:credentials] << "Invalid username or password"
+            false
+          else
+            true
+        end
+      end
+    end
+
     def validate_client_id
       begin
         @client_application = Oauth2ClientApplication.find_by_client_id @client_id
@@ -65,19 +130,19 @@ module OAUTH2
     end
 
     def validate_redirect_uri
-      errors[:invalid_request] = []
-      if redirect_uri.nil?
-        errors[:invalid_request] << "Redirect uri is not valid. Provide and absolute URI"
+      errors[:redirect_uri] = []
+      if @redirect_uri.nil?
+        errors[:redirect_uri] << "Redirect uri is not valid. Provide and absolute URI"
       else
         uri = Addressable::URI.parse(@redirect_uri)
         unless ["http", "https"].include? uri.scheme
-          errors[:invalid_request] << "uri scheme is missing or unsupported"
+          errors[:redirect_uri] << "uri scheme is missing or unsupported"
         end
         unless uri.fragment.nil?
-          errors[:invalid_request] << "malformed uri must not include URI fragment"
+          errors[:redirect_uri] << "malformed uri must not include URI fragment"
         end
       end
-      errors[:invalid_request].any? ? false : true
+      errors[:redirect_uri].any? ? false : true
     end
 
     def validate_response_type
@@ -86,9 +151,14 @@ module OAUTH2
       false
     end
 
-    def client_application
-      return @client_application unless @client_application.nil?
-      validate_client_id
+    def validate_grant_type
+      return true if GRANT_TYPES.include? @grant_type
+      @errors[:grant_type] = "UnsupportedGrantType"
+      false
+    end
+
+    def validate_scope(&block)
+      raise "Fix Me"
     end
 
     def generate_authorization_code
@@ -99,6 +169,16 @@ module OAUTH2
     def generate_request_token
       raise unless client_application
       client_application.generate_request_token 
+    end
+
+    def generate_access_token
+      raise unless client_application
+      client_application.generate_access_token
+    end
+
+    def generate_refresh_token
+      raise unless client_application
+      client_application.generate_refresh_token
     end
   end
 end
