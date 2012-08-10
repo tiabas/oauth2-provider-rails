e403ae598e4799b428b40ab65ba9bf563dca8cc7
Kevin Mutyaba <kmutyaba@yammer-inc.com>
2012-08-09 20:21:46 -0700

    test preparation

--- test/functional/oauth2_controller_test.rb
+++ test/functional/oauth2_controller_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class Oauth2ControllerTest < ActionController::TestCase
+  # implicit grant
+
+  # step 1: authorize
+  # - invalid response type
+  # - invalid grant type
+  # - invalid redirect URI 
+  # - unknown client id
+  # - known client id
+  # - force dialog
+
   test "should get authorize" do
     get :authorize
     assert_response :success
